b56294333d7850e420c5888925108aa993ad8a2e
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-04 11:38:25 +0530

    session24-project-2 input css

--- Project-2/script.js
+++ Project-2/script.js
@@ -1,5 +1,17 @@
-function addToDo(){
-    let input = document.getElementsByClassName("input");
-    input.innerHtml = value;
+let input = document.getElementById("input");
+let list = document.getElementById("list-container");
+
+function addTask(){
+    if(input.value ===''){
+        alert('You must write something!')
+    }else{
+        let li = document.createElement("li");
+        li.innerHtml = input.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "/u00d7";
+        li.appendChild(span);
+    }
+    input.value = ''
    
 }
