1dfd7962392dde44a824df194ec36cb2ae2b0b10
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-17 10:46:23 +0530

    feat: session 28-syncasync.js file added

---
+++ Node/Session28/syncasync.js
@@ -0,0 +1,24 @@
+var fs=require('fs');
+
+//01 async
+console.log("Starts....");
+fs.readFile('files/file2.txt',function(err,data){
+    if(err)
+        throw err;
+    else
+        console.log("Data:",data.toString());
+})
+console.log("Ends....");
+
+
+//02 sync
+console.log("Starts....");
+try {
+
+    fs.writeFileSync('files/file2.txt',"Synchronous Way of Writting File");
+    console.log("Data Added Successfully")
+
+} catch (error) {
+    console.log("Error:",error)
+}
+console.log("Ends....");
