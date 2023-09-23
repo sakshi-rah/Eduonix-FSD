cfe311e89675832caa983af7f250bad37b1a5bc4
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-23 20:03:17 +0530

    feat: Session 29 added

---
+++ Node/Session29/writeStream.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var data = "I am sakshi rahangdale, Full stack developer!"
+
+var writestream = fs.createWriteStream('./File/file.txt');
+writestream.write(data);
+writestream.end();
+writestream.on('finish',function(){
+    console.log('data added');
+})
+writestream.on('error',function(error){
+    console.log(error.stack);
+})
+console.log('Complete Program');
