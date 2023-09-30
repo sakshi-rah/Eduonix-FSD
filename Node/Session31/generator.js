7a885137a0cb48809a83c66f5314944a062decf9
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-09-30 20:02:56 +0530

    feat: Session31 files added

---
+++ Node/Session31/generator.js
@@ -0,0 +1,13 @@
+function* numberGenrator(){
+    let num=1;
+    while(true){
+        yield num;
+        num++;
+    }
+}
+
+const myGenrator=numberGenrator();
+console.log(myGenrator.next().value);
+console.log(myGenrator.next().value);
+console.log(myGenrator.next().value);
+console.log(myGenrator.next().value);
