c6050c806bf9b0bab42ffbb36a9edd9afa7160c7
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-10-14 20:52:40 +0530

    feat: Session35-hook-useState added in firstapp

--- ReactJS/Session35/firstapp/src/Componants/UserDetails.js
+++ ReactJS/Session35/firstapp/src/Componants/UserDetails.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 function UserDetails() {
     const [name, setName] = useState('Sakshi')
-    const [age] = useState(22)
+    const [age, setAge] = useState(22)
 
     return (
         <>
             <div className="fs-3 text-warning p-1">
                 <p>My name is {name}</p>
                 <p>age: {age}</p>
-                <button onClick={()=>setName("Mukesh")} className="btn btn-dark">Change Name</button>
+                <button onClick={()=>{setName("Mukesh"); setAge("21-10m")}} className="btn btn-dark">Change Name</button>
             </div>
         </>
     )
