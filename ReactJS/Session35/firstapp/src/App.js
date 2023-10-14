8dbf7d7d22ebd8a95bfa2034fc4cf061ccaa3f93
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-10-14 21:21:19 +0530

    feat: Session35 props added

--- ReactJS/Session35/firstapp/src/App.js
+++ ReactJS/Session35/firstapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Componants/Header';
+
 import "bootstrap/dist/css/bootstrap.min.css" 
 import "bootstrap/dist/js/bootstrap.bundle.min"
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Hello Sakshi Rahangdale!</h1>
-      <Header/>
+      <Header title="Learn about React Props"/>
     </div>
   );
 }
--- ReactJS/Session35/firstapp/src/Componants/Header.js
+++ ReactJS/Session35/firstapp/src/Componants/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import UserDetails from './UserDetails'
 // import './Header.css'
-function Header() {
-  return (
-    <div className='headerSection bg-success text-white p-2 m-5'>
-        <h2>  This is Header Section. </h2>
-        <UserDetails/>
-    </div>
-  )
+function Header({ title }) {
+    return (
+        <div className='headerSection bg-success text-white p-2 m-5'>
+            <h2>  This is Header Section. </h2>
+            <p className='text-info fs-5'><span className='fs-4 text-warning'>Today Title:</span> {title}</p>
+            <UserDetails />
+        </div>
+    )
 }
 
 export default Header
