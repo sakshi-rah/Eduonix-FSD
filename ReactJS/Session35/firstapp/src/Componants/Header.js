73453bfd300effb1aaafba4875b651d26e3156d9
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-10-15 12:10:17 +0530

    feat: Session36 added

--- ReactJS/Session35/firstapp/src/Componants/Header.js
+++ ReactJS/Session35/firstapp/src/Componants/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserDetails from './UserDetails'
+import {Button} from 'react-bootstrap'
 // import './Header.css'
 function Header({ title }) {
     return (
@@ -7,6 +8,7 @@ function Header({ title }) {
             <h2>  This is Header Section. </h2>
             <p className='text-info fs-5'><span className='fs-4 text-warning'>Today Title:</span> {title}</p>
             <UserDetails />
+            <Button variant='danger'>ReactBootstrap Btn</Button>
         </div>
     )
 }
--- ReactJS/Session36/reactapp2/src/App.js
+++ ReactJS/Session36/reactapp2/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h3>Hello </h3>
-    
+      <h1>Today Topic is React Bootstrap</h1>
     </div>
   );
 }
